218688a8e636e24a06632ef96ced77ed3dd6e6ca
Stephen Simpson <me@simpo.org>
2014-04-28 15:08:33 +0100

    Added manual bootstrapping so more than one app can be run.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ *  @author Stephen Simpson <stephen.simpson@redcar-cleveland.gov.uk>
+ *  @license MIT
+ *  @copyright Redcar & cleveland Borough Council 2014
+ *  @version 0.0.1
+ */
+
+console.log(1);
+var Cal = angular.module("Cal", []);
+console.log(2);
